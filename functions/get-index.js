3d223ed20672276f200ad1704edc2db2c1779ca9
Zeihan Aulia <zeihan@warungpintar.co>
2019-04-18 20:30:36 +0700

    show restaurans

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,26 +1,39 @@
 /* jshint node: true */
-'use strict';
+"use strict";
 
 const co = require("co");
 const Promise = require("bluebird");
 const fs = Promise.promisifyAll(require("fs"));
+const Mustache = require("mustache");
+const http = require('superagent-promise')(require('superagent'), Promise);
+const restauranApiRoot = process.env.restaurant_api;
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 var html;
 
 function* loadHtml() {
   if (!html) {
-    html = yield fs.readFileAsync('static/index.html', 'utf-8');
+    html = yield fs.readFileAsync("static/index.html", "utf-8");
   }
   return html;
 }
 
-module.exports.handler = co.wrap(function* (event) {
-  let html = yield loadHtml();
+
+function* getRestaurants() {
+  return (yield http.get(restauranApiRoot)).body;
+}
+
+module.exports.handler = co.wrap(function*(event) {
+  let template = yield loadHtml();
+  let restaurants = yield getRestaurants();
+  let dayOfWeek = days[new Date().getDay()];
+  let html = Mustache.render(template, { dayOfWeek, restaurants });
+
   return {
     statusCode: 200,
     body: html,
     headers: {
-      'Content-Type': 'text/html; charset=UTF-8'
+      "Content-Type": "text/html; charset=UTF-8"
     }
   };
 });
