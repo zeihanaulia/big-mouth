37e20f2c4ff24ed855f718f506160ad15ea488fc
Zeihan Aulia <zeihan@warungpintar.co>
2019-04-18 21:57:07 +0700

    add security get restaurants api

--- functions/get-index.js
+++ functions/get-index.js
@@ -5,9 +5,19 @@ const co = require("co");
 const Promise = require("bluebird");
 const fs = Promise.promisifyAll(require("fs"));
 const Mustache = require("mustache");
-const http = require('superagent-promise')(require('superagent'), Promise);
+const http = require("superagent-promise")(require("superagent"), Promise);
+const aws4 = require("aws4");
+const URL = require("url");
 const restauranApiRoot = process.env.restaurant_api;
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 
 var html;
 
@@ -18,9 +28,21 @@ function* loadHtml() {
   return html;
 }
 
-
 function* getRestaurants() {
-  return (yield http.get(restauranApiRoot)).body;
+  let url = URL.parse(restauranApiRoot);
+  let opts = {
+    host: url.hostname,
+    path: url.pathname
+  };
+
+  aws4.sign(opts);
+
+  return (yield http
+    .get(restauranApiRoot)
+    .set("Host", opts.headers.Host)
+    .set("X-Amz-Date", opts.headers["X-Amz-Date"])
+    .set("Authorization", opts.headers.Authorization)
+    .set("X-Amz-Security-Token", opts.headers["X-Amz-Security-Token"])).body;
 }
 
 module.exports.handler = co.wrap(function*(event) {
